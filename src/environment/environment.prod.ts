f57bb79a7bee4b11c09d68f12470b28b8427c558
daniel vera <manuelcarrillopenuela51@gmail.com>
2024-10-06 10:37:18 -0500

    feat:config firebase

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "tu_api_key_prod",
+    authDomain: "tu_auth_domain_prod",
+    projectId: "tu_project_id_prod",
+    storageBucket: "tu_storage_bucket_prod",
+    messagingSenderId: "tu_messaging_sender_id_prod",
+    appId: "tu_app_id_prod"
+  }
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "tu_api_key",
+    authDomain: "tu_auth_domain",
+    projectId: "tu_project_id",
+    storageBucket: "tu_storage_bucket",
+    messagingSenderId: "tu_messaging_sender_id",
+    appId: "tu_app_id"
+  }
+};
